d350c5d01adbd753f09c12601d7ab7dcb00718a3
vabbalareddymohan <vabbalareddymohan20@gmail.com>
2025-02-22 17:20:33 +0530

    project created

---
+++ src/test/java/com/gitMethods/sample.java
@@ -0,0 +1,17 @@
+package com.gitMethods;
+
+public class sample {
+	private void method1() {
+System.out.println("hyd");
+	}
+	private void method2() {
+		System.out.println("che");
+
+	}
+	private void method3() {
+		System.out.println("ap");
+
+	}
+	
+
+}
